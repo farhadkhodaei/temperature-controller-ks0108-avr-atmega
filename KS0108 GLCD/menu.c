63db8ff6925600572bdc0df15e7938cab4c8ba64
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2019-03-15 16:10:24 +0330

    Add files via upload

--- KS0108 GLCD/menu.c
+++ KS0108 GLCD/menu.c
@@ -1634,7 +1634,7 @@ DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<
 PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
 
 // Port C initialization
-// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+// Function: Bit7=In Bit6=In Bit5=In Bit4=Out Bit3=In Bit2=In Bit1=In Bit0=In 
 DDRC=0x00;
 DDRC|=1<<4;
 // State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
